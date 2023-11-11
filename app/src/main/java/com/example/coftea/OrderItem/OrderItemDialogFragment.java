f4ce4caecef8d4285866d75c53c178e9a06fa14d
UVMapping <115959246+uvmapping@users.noreply.github.com>
2023-11-11 17:31:57 +0800

    Added Advance Order Page, with add to cart function
    Signed-off-by: KendallRey <107897863+KendallRey@users.noreply.github.com>

--- app/src/main/java/com/example/coftea/OrderItem/OrderItemDialogFragment.java
+++ app/src/main/java/com/example/coftea/OrderItem/OrderItemDialogFragment.java
@@ -176,6 +176,7 @@ public class OrderItemDialogFragment extends DialogFragment {
 
     @Override
     public void onHiddenChanged(boolean hidden) {
+
         if(hidden) {
             this.orderItemDialogViewModel.clearOrderItem();
         }
